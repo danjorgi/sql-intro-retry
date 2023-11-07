7b4d95ddd8865b074815f784f28a6cd3c6cbffc8
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-11-06 23:29:53 -0700

    Employee complete

---
+++ employee.sql
@@ -0,0 +1,5 @@
+SELECT first_name, last_name FROM employee WHERE city = 'Calgary';
+SELECT birth_date FROM employee ORDER BY birth_date DESC LIMIT 1;
+SELECT birth_date FROM employee ORDER BY birth_date ASC LIMIT 1;
+SELECT employee_id, first_name, last_name FROM employee WHERE reports_to = (SELECT employee_id FROM employee WHERE last_name = 'Edwards' AND first_name = 'Nancy');
+SELECT COUNT(*) FROM employee WHERE city = 'Lethbridge';
